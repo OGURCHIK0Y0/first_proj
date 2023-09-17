50850b0a064090513ab14c714023a9759186e522
OGURCHIK0Y0 <145265584+OGURCHIK0Y0@users.noreply.github.com>
2023-09-17 15:03:50 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+q=int(input())
+# w=(input())
+# e=(input())
+# r=(input())
+# t=(input())
+# y=(input())
+# n=(input())
+# b=(input())
+# c=(input())
+if q == 0:
+    print('ввод')
+elif q == 1:
+    print('вывод')
+elif q == 1:
+    print('вывод')
